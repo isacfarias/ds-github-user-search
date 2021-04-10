77c7970bd2d119dd48fbf0cd0eacb44d18193a5b
isacfarias <isac.farias.21@gmail.com>
2021-04-09 22:08:14 -0400

    add link na navbar

--- src/core/components/Navbar/index.tsx
+++ src/core/components/Navbar/index.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './styles.css';
 
 const Navbar = () => (
     <nav className="nav-main-menu">
         <div className="nav-main-title">
-            <h4 className="nav-title">Bootcamp DevSuperior</h4>
+            <Link to="/" className="nav-logo-text">
+                <h4 className="nav-title">Bootcamp DevSuperior</h4>
+            </Link>
         </div>
     </nav>
 );
